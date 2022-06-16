4f0e25705e1b13fafff0a15513bea340bb246021
Patrick Viry <patrickviry@moderne.io>
2022-06-16 17:32:50 +0200

    Scaffolding for the HTTP example.

---
+++ rewrite-java/src/main/java/org/openrewrite/java/dataflow2/examples/HttpAnalysis.java
@@ -0,0 +1,6 @@
+//package org.openrewrite.java.dataflow2.examples;
+//
+//import org.openrewrite.java.dataflow2.ValueAnalysis;
+//
+//public class HttpAnalysis extends ValueAnalysis<HttpAnalysisValue> {
+//}
---
+++ rewrite-java/src/main/java/org/openrewrite/java/dataflow2/examples/HttpAnalysisValue.java
@@ -0,0 +1,16 @@
+//package org.openrewrite.java.dataflow2.examples;
+//
+//import org.openrewrite.java.dataflow2.Joiner;
+//
+//public class HttpAnalysisValue {
+//
+//    // Secure, NonSecure
+//    // WeDontKnow
+//    // Conflict
+//
+//    // WeDontKnow < Secure, NonSecure < Conflict
+//
+//    public static final Joiner<ZipSlipValue> JOINER = new Joiner<ZipSlipValue>() {
+//
+//    }
+//}
